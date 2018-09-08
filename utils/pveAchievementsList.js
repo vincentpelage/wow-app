e1689367345fb03e6e38000d85490125d7152a84
vincentpelage <vincent.plge@gmail.com>
2018-09-08 15:58:04 +1000

    pveAchievementsList added

---
+++ utils/pveAchievementsList.js
@@ -0,0 +1,166 @@
+const pveAchievementsList = [
+    {
+        name: "Atal'Dazar",
+        id: 12824,
+        type: "normalDungeon",
+        desc: "Defeat Yazma in Atal'Dazar"
+    },
+    {
+        name: "Freehold",
+        id: 12831,
+        type: "normalDungeon",
+        desc: "Defeat Harlan Sweete in Freehold."
+    },
+    {
+        name: "Shrine of the Storm",
+        id: 12835,
+        type: "normalDungeon",
+        desc: "Defeat Vol'zith the Whisperer in Shrine of the Storm."
+    },
+    {
+        name: "The Temple of Sethraliss",
+        id: 12504,
+        type: "normalDungeon",
+        desc: "Restore the Avatar of Sethraliss in the Temple of Sethraliss."
+    },
+    {
+        name: "The MOTHERLODE!!",
+        id: 12844,
+        type: "normalDungeon",
+        desc: "Defeat Mogul Razdunk in The MOTHERLODE!!"
+    },
+    {
+        name: "The Underrot",
+        id: 12500,
+        type: "normalDungeon",
+        desc: "Defeat the Unbound Abomination in Underrot."
+    },
+    {
+        name: "Tol Dagor",
+        id: 12840,
+        type: "normalDungeon",
+        desc: "Defeat Overseer Korgus in Tol Dagor."
+    },
+    {
+        name: "Waycrest Manor",
+        id: 12483,
+        type: "normalDungeon",
+        desc: "Defeat Gorak Tul in Waycrest Manor."
+    },
+    {
+        name: "Atal'Dazar",
+        id: 12825,
+        type: "heroicDungeon",
+        desc: "Defeat Yazma in Atal'Dazar on Heroic difficulty or higher."
+    },
+    {
+        name: "Freehold",
+        id: 12832,
+        type: "heroicDungeon",
+        desc: "Defeat Harlan Sweete in Freehold on Heroic difficulty or higher."
+    },
+    {
+        name: "Shrine of the Storm",
+        id: 12837,
+        type: "heroicDungeon",
+        desc:
+            "Defeat Vol'zith the Whisperer in Shrine of the Storm on Heroic difficulty or higher."
+    },
+    {
+        name: "The Temple of Sethraliss",
+        id: 12505,
+        type: "heroicDungeon",
+        desc:
+            "Restore the Avatar of Sethraliss in the Temple of Sethraliss on Heroic difficulty or higher."
+    },
+    {
+        name: "The MOTHERLODE!!",
+        id: 12845,
+        type: "heroicDungeon",
+        desc:
+            "Defeat Mogul Razdunk in The MOTHERLODE!! on Heroic difficulty or higher."
+    },
+    {
+        name: "The Underrot",
+        id: 12501,
+        type: "heroicDungeon",
+        desc:
+            "Defeat the Unbound Abomination in Underrot on Heroic difficulty or higher."
+    },
+    {
+        name: "Tol Dagor",
+        id: 12841,
+        type: "heroicDungeon",
+        desc: "Defeat Overseer Korgus in Tol Dagor on Heroic difficulty or higher."
+    },
+    {
+        name: "Waycrest Manor",
+        id: 12484,
+        type: "heroicDungeon",
+        desc: "Defeat Gorak Tul in Waycrest Manor on Heroic difficulty or higher."
+    },
+    {
+        name: "Atal'Dazar",
+        id: 12826,
+        type: "mythicDungeon",
+        desc: "Defeat Yazma in Atal'Dazar on Mythic difficulty."
+    },
+    {
+        name: "Freehold",
+        id: 12833,
+        type: "mythicDungeon",
+        desc: "Defeat Harlan Sweete in Freehold on Mythic difficulty."
+    },
+    {
+        name: "King's Rest",
+        id: 12848,
+        type: "mythicDungeon",
+        desc: "Defeat Dazar in Kings' Rest."
+    },
+    {
+        name: "Shrine of the Storm",
+        id: 12838,
+        type: "mythicDungeon",
+        desc:
+            "Defeat Vol'zith the Whisperer in Shrine of the Storm on Mythic difficulty."
+    },
+    {
+        name: "Siege of Boralus",
+        id: 12847,
+        type: "mythicDungeon",
+        desc: "Defeat Viq'goth in Siege of Boralus."
+    },
+    {
+        name: "The Temple of Sethraliss",
+        id: 12506,
+        type: "mythicDungeon",
+        desc:
+            "Restore the Avatar of Sethraliss in the Temple of Sethraliss on Mythic difficulty."
+    },
+    {
+        name: "The MOTHERLODE!!",
+        id: 12846,
+        type: "mythicDungeon",
+        desc: "Defeat Mogul Razdunk in The MOTHERLODE!! on Mythic difficulty."
+    },
+    {
+        name: "The Underrot",
+        id: 12502,
+        type: "mythicDungeon",
+        desc: "Defeat the Unbound Abomination in Underrot on Mythic difficulty."
+    },
+    {
+        name: "Tol Dagor",
+        id: 12842,
+        type: "mythicDungeon",
+        desc: "Defeat Overseer Korgus in Tol Dagor on Mythic difficulty."
+    },
+    {
+        name: "Waycrest Manor",
+        id: 12488,
+        type: "mythicDungeon",
+        desc: "Defeat Gorak Tul in Waycrest Manor on Mythic difficulty."
+    }
+];
+
+module.exports.pveAchievementsList = pveAchievementsList;
