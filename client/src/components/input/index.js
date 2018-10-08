0984a703a3db8fd232c467a4da8337588aef2a94
Charlotte Cady <charlottecady@MacBook-Pro-de-Charlotte.local>
2018-10-08 10:24:37 +0900

    ajout  select component

--- client/src/components/input/index.js
+++ client/src/components/input/index.js
@@ -1,37 +1,50 @@
 import React from "react";
 import styled from "styled-components";
-import { getInputStyles, getInnerStyle } from "./inputStyles";
+import { getInputStyles, getInnerStyle } from "./styles";
 
-const Input = ({ cursor, width, defaultValue, placeholder, type, value }) => {
-  const InputWrapper = styled.div`
-    ${getInputStyles};
-  `;
+const InputWrapper = styled.div`
+  ${getInputStyles};
+`;
 
-  // The actual input element
-  const InputElement = styled.input`
-    flex-grow: 1;
-    background: none;
-    border: none;
-    outline: none;
-    transition: box-shadow 200ms ease-in-out;
-    transition-delay: 50ms;
-    ${getInnerStyle};
-  `;
+// The actual input element
+const InputElement = styled.input`
+  flex-grow: 1;
+  background: none;
+  border: none;
+  outline: none;
+  transition: box-shadow 200ms ease-in-out;
+  transition-delay: 50ms;
+  ${getInnerStyle};
+`;
 
-  return (
-    <InputWrapper cursor={cursor} width={width}>
-      <InputElement
-        defaultValue={defaultValue}
-        placeholder={placeholder}
-        onMouseDown={e => {
-          // If selected directly don't refocus.
-          e.stopPropagation();
-        }}
-        type={type}
-        value={value}
-      />
-    </InputWrapper>
-  );
-};
+class Input extends React.Component {
+  render() {
+    const {
+      cursor,
+      width,
+      defaultValue,
+      placeholder,
+      type,
+      value,
+      onChange
+    } = this.props;
+
+    return (
+      <InputWrapper cursor={cursor} width={width}>
+        <InputElement
+          defaultValue={defaultValue}
+          placeholder={placeholder}
+          onMouseDown={e => {
+            // If selected directly don't refocus.
+            e.stopPropagation();
+          }}
+          type={type}
+          value={value}
+          onChange={onChange}
+        />
+      </InputWrapper>
+    );
+  }
+}
 
 export default Input;
