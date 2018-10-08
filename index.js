4dbd5f7eb4d5e0fd39018d65c6bf4960b913ed06
vincentpelage <vincent.plge@gmail.com>
2018-10-08 14:20:27 +1100

    get dungeon data from api ok

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const getDungeonAchievement = require("./controllers/getDungeonAchievement");
 const getRaidAchievement = require("./controllers/getRaidAchievement");
 
 const app = express();
+
+// allowing Access-Control-Allow-Origin
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+});
+
+// Allow request parsing
+app.use(bodyParser.json());
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
@@ -14,7 +31,7 @@ app.get('/', (req, res) => {
     res.send('Express Server Launched')
 });
 
-app.get('/dungeonsAchievements', getDungeonAchievement);
+app.post('/dungeonsAchievements', getDungeonAchievement);
 app.get('/raidsAchievements', getRaidAchievement);
 
 // The "catchall" handler: for any request that doesn't
